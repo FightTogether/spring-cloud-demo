8349b521a74caf0a0226c8c4de078c89d9930a45
FightTogether <24369952@qq.com>
2018-03-28 21:40:23 +0800

    添加eureka-client模块

---
+++ spring-cloud-demo-eureka-client/src/test/java/cn/why360/eurekaclient/SpringCloudDemoEurekaClientApplicationTests.java
@@ -0,0 +1,16 @@
+package cn.why360.eurekaclient;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringCloudDemoEurekaClientApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
--- spring-cloud-demo-eureka-server/src/main/java/cn/why360/eurekaserver/SpringCloudDemoEurekaServerApplication.java
+++ spring-cloud-demo-eureka-server/src/main/java/cn/why360/eurekaserver/SpringCloudDemoEurekaServerApplication.java
@@ -7,9 +7,9 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @EnableEurekaServer
 @SpringBootApplication
-public class EurekaserverApplication {
+public class SpringCloudDemoEurekaServerApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(EurekaserverApplication.class, args);
+		SpringApplication.run(SpringCloudDemoEurekaServerApplication.class, args);
 	}
 }
