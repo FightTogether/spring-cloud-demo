7d119b5747d3b88ba3fbfd3a304942d102cb661f
FightTogether <silentpoppy@foxmail.com>
2018-04-08 14:56:35 +0800

    zuul

---
+++ spring-cloud-demo-service-zuul/src/test/java/cn/why360/servicezuul/SpringCloudDemoServiceZuulApplicationTests.java
@@ -0,0 +1,11 @@
+package cn.why360.servicezuul;
+
+/*@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringCloudDemoServiceZuulApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+}
+*/
