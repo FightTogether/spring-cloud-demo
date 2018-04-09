1c6212ae00ca5f75532e1af662eeb00e9e4023fc
FightTogether <silentpoppy@foxmail.com>
2018-04-09 10:50:34 +0800

    分布式配置中心

---
+++ spring-cloud-demo-config-server/src/main/java/cn/why360/configserver/SpringCloudDemoConfigServerApplication.java
@@ -0,0 +1,14 @@
+package cn.why360.configserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+@SpringBootApplication
+@EnableConfigServer
+public class SpringCloudDemoConfigServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCloudDemoConfigServerApplication.class, args);
+	}
+}
